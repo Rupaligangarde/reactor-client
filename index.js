9e34d7d519e3d180a52172e053c2d32af51f7f9a
Rupali Gangarde <rgangarde@falabella.cl>
2021-05-11 13:59:11 +0530

    Rupali | .gitignore

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var app = express();
+app.use(express.json())
 
-app.get('/stocks/:id', function (req, res) {
+app.get('/stocks', function (req, res) {
 
-  const okResponse = ["1001", "1002","1003", "1004", "1005", "1006", "1007", "1008", "1009", "1010"];
+  const okResponse = [
+    "1001", "1002","1003", "1004", "1005", "1006", "1007", "1008", "1009", "1010", 
+    "4001", "4002", "4003", "4004", "4005", "4006", "4007", "4008", "4009", "4010",
+    "5001", "5002", "5003", "5004", "5005", "5006", "5007", "5008", "5009", "5010",
+    "6001", "6002", "6003", "6004", "6005", "6006", "6007", "6008", "6009", "6010"
+  ];
+
+
   const conflictResponse = ["2001", "2002","2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010"];
   const notFoundResponse = ["3001", "3002","3003", "3004", "3005", "3006", "3007", "3008", "3009", "3010"];
 
@@ -46,13 +54,24 @@ app.get('/stocks/:id', function (req, res) {
     }
 ];
 const conflictRes={"errors":[{"code":"Conflict","status":409,"detail":"Data exists with sourceUpdatedAt: 2021-05-12T12:23:00.000Z Please try again with the updated data"}]};
-  const id = req.params.id;
+  const id = req.query.variantId;
+  console.log(id);
   if(okResponse.includes(id)){
     res.status(200).send(okRes);
-  } else if(conflictResponse.includes(id)){
-    res.status(409).send(conflictRes);
-  } else res.sendStatus(404);
+  } else {
+    res.sendStatus(404);
+  }
 });
+
+
+app.post("/stocks", function(req, res){
+  const id = req.query.variantId;
+  if(conflictResponse.includes(id)){
+    res.status(409).send(conflictRes);
+  }
+  else res.status(200).send(req.body);
+});
+
 
 app.listen(3000, function () {
   console.log('Catalog server app listening on port 3000!');
